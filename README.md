15158bf2c8deb64d4c1030fb651c18efa2dec621
Faisal1244311 <103377014+Faisal1244311@users.noreply.github.com>
2022-06-10 11:21:05 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# script
-Yoi
+//SUBSCRIBE HERMAN CHANEL
---
+++ config.js
@@ -0,0 +1,40 @@
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'ad9e14dd7c',
+}
+
+// Other
+global.owner = ['6283134409906']
+global.packname = 'OP᭄★𝑭𝒂𝒊𝒔𝒂𝒍 𝑩𝒐𝒕𝒛 ⦖'
+global.author = 'by Faisal nawar'
+global.sessionName = 'HermanChanel'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+}
+global.thumb = fs.readFileSync('./lib/hermanChnl.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
